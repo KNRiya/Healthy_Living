c9d04936886ad4ffa1eeb1e8b6b24a4d0cdaf45a
KNRiya <khayrun.nahar@northsouth.edu>
2020-09-26 02:44:29 +0600

    Location

--- app/src/main/java/com/example/healthy_living/Map.java
+++ app/src/main/java/com/example/healthy_living/Map.java
@@ -2,13 +2,26 @@ package com.example.healthy_living;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Map extends AppCompatActivity {
-
+private Button mapToLaca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mapToLaca=findViewById(R.id.mapToLocation);
+        mapToLaca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Map.this,Location.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
