409503a9fd32f5ea5955294f025953ab2ccca21f
Foysal_Hossain <mdfoysal.hossain@northsouth.edu>
2020-09-27 17:45:42 +0600

    Code Update By MD Foysal

--- app/src/main/java/com/example/healthy_living/Appoinments.java
+++ app/src/main/java/com/example/healthy_living/Appoinments.java
@@ -2,13 +2,34 @@ package com.example.healthy_living;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Appoinments extends AppCompatActivity {
+    private Button Dermatology;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinments);
+        Dermatology = (Button) findViewById(R.id.Dermatology);
+        Dermatology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDermatology();
+
+            }
+        });
     }
-}
+    public void openDermatology(){
+        //Intent intent = new Intent( this,Info.class);
+       // startActivity(intent);
+    }
+    public void onDermatologyBtnClick( View view ){
+        Intent intent = new Intent( this,Info.class);
+        startActivity(intent);
+
+    }
+    }
--- app/src/main/java/com/example/healthy_living/Reminders.java
+++ app/src/main/java/com/example/healthy_living/Reminders.java
@@ -2,13 +2,36 @@ package com.example.healthy_living;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Reminders extends AppCompatActivity {
+    private Button MedicineTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+        MedicineTime = (Button) findViewById(R.id.MedicineTime);
+        MedicineTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMedicineTime();
+
+            }
+        });
+
+        });
+    }
+    public void openMedicineTime(){
+
+
+    }
+    public void onMedicineTimeBtnClick( View view ){
+        //Intent intent = new Intent( this,Info.class);
+       // startActivity(intent);
+
     }
 }
